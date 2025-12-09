2e86e727cbc769e9f5080963442d07a02732eb8f
Maximo Winfield <168515939+maximowinfield@users.noreply.github.com>
2025-12-09 16:00:52 -0500

    Set base path for GitHub Pages deployment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Microsoft-Fullstack-Sample/', // required for GitHub Pages
 })
